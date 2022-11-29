640418e2b61032355ee54fbfcb05c1ba9c71c23c
18-Wesley <weesleymarques2003@gmail.com>
2022-11-29 20:29:55 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function vereficar (){
+    let input = document.querySelector("#nome");
+
+    let texto = input.value;
+    if (texto == 'Wesley'){
+        alert('Bem vindo de Volta Senhor ' + usuario)
+    }else{
+        alert('Olá '+ usuario )
+    }
+
+}
+
+
